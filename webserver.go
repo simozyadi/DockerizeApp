6ee005e560f227932e6f0671140d43d592e73b2e
Mohammed ZYADI <medzyadi@gmail.com>
2021-01-15 22:07:48 +0100

    simple server goapp

---
+++ webserver.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hey, Go is beautifull")
+}
+
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hey, Your are in home page</h1> ")
+	fmt.Fprintf(w, "<p> go is fast</p> ")
+	fmt.Fprintf(w, `
+	<p> i agree with you that go is fast</p>
+	<p>you %s even add %s</p> `, "can", "<strong>variables</strong>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/home", homepageHandler)
+	http.ListenAndServe("0.0.0.0:8080", nil)
+	http.ListenAndServe("0.0.0.0:8080", nil);
+}
